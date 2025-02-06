91e7a488ed827d010a5e09c1d1ac7c58fe381a09
aaru4 <98666654+aaru4@users.noreply.github.com>
2025-02-05 22:40:44 -0500

    Update ProjectCard.tsx

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -16,7 +16,7 @@ function hexToRgba(hex: string, opacity: number) {
 export const ProjectBox: React.FC<ProjectCardProps> = ({ title, org, date, desc, color }) => {
   return (
     <div
-      className={`w-[250px] p-6 rounded-lg shadow-lg mx-auto transform transition-transform duration-300 hover:scale-110`}
+      className={`w-[250px] h-[400px] p-6 rounded-lg shadow-lg mx-auto transform transition-transform duration-300 hover:scale-110`}
       style={{
         background: `linear-gradient(${hexToRgba(color, 0.5)}, ${color})`,
       }}
